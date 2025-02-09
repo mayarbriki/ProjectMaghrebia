27f5cc4ad9983bda5353214ae1d7e27b3a1fc5af
mayarbriki <100524582+mayarbriki@users.noreply.github.com>
2025-02-09 20:54:15 +0100

    Integration template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  currentPage: string = 'index'; // Example value, update as needed
+}
